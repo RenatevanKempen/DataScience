6b819566ee5e60fdba0ef10fa7595b5445ad41d0
RenatevanKempen <47600826+RenatevanKempen@users.noreply.github.com>
2020-03-27 09:15:19 +0100

    New game!!

--- OOP/Room with character game/main.py
+++ OOP/Room with character game/main.py
@@ -27,7 +27,7 @@ bill.set_weakness("money")
 ballroom.set_character(bill)
 
 lisa = Friend("Lisa", "a kind and warm person")
-lisa.set_kindness("Knuffels")
+lisa.set_conversation("Hi there, do you want a hugg?")
 kitchen.set_character(lisa)
 
 current_room = kitchen          
@@ -46,20 +46,27 @@ while keepGoing:
       current_room = current_room.move(command)
     elif command == "talk":
       inhabitant.talk()
-   # elif inhabitant == lisa:
-  #    print("what would you like to give " + inhabitant)
-  #    nice = input()
-  #    y = inhabitant.hugg(nice)
-    else:
-      print("What will you fight with?")
-      fight_with = input()
-      x  = inhabitant.fight(fight_with)
-      if x == False:
-        print("GAME OVER")         
-        keepGoing = False
+    elif command == "hugg":
+      if inhabitant == None:
+        print("there is no one here to hug:(")
+      elif isinstance(inhabitant, Enemy):
+        print("I wouldn't do this if I were you..... WHOHAHA!")
       else:
-        print("Well done, you have slayed the ENEMY!!")
-        keepGoing = True
+        print("hi, my name is " + inhabitant.name + " and I will give you a bigg hugg back")
+    else:
+      if inhabitant == None or isinstance(inhabitant, Friend):
+        print("there is no on here to fight with! Continue to the next room")
+      else:
+        print("What will you fight with?")
+        fight_with = input()
+        x  = inhabitant.fight(fight_with)
+        if x == False:
+         print("GAME OVER")         
+         keepGoing = False
+        else:
+         print("Well done, you have slayed the ENEMY!!")
+         current_room.set_character(None)
+         keepGoing = True
 
         
         
